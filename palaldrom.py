32146f851df0f4a00a5412b70607c291ae9be798
Pramodkumarsahu <pks7381446602@gmail.com>
2021-11-25 16:40:39 +0530

    first commit

---
+++ palaldrom.py
@@ -0,0 +1,21 @@
+#WAPP TO CHECK  IF A GIVEN STRING IS PALINDROME OR NOT 
+
+str1=input('enter any string')
+
+if str1== str1[-1:-(len(str1)+1):-1]:
+   print('string is palindrom')
+else:
+   print('string is not palindrom')
+
+
+num=int(input("enter any number"))
+temp=num
+res=0
+while num>0:
+   r=num%10
+   res=res*10+r
+   num//=10
+if temp ==res:
+   print('num is palindrom')
+else:
+   print('num is not palindrome')
